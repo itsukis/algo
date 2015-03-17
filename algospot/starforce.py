e04cdb0e3a3496a4f755aeb61dba65d5819d7ae9
jiyoung song <itsukis@gmail.com>
2015-03-17 10:29:08 +0900

    debugging

--- algospot/starforce.py
+++ algospot/starforce.py
@@ -1,15 +1,6 @@
 ## https://algospot.com/judge/problem/read/STARFORCE
 
 import sys
-'''
-N = 6
-M = 2
-A = [8, 7, 4, 12, 2, 1]
-
-N = 3
-M = 2
-A = [4, 2, 1]
-'''
 
 def bitwise_or(A):
 	bitwiseOR = 0
@@ -17,38 +8,53 @@ def bitwise_or(A):
 		bitwiseOR = bitwiseOR | i	
 	return bitwiseOR
 
-def count_bitwise_or(A):
+def count_setbit(A):
 	bitwiseOR = bitwise_or(A)
-	return bitwiseOR.bit_length()
+	return bin(bitwiseOR).count("1")
 
-def possible_max_bit(bit, A, M):
+def find_max_setbit(bit, A, M):
+
 	if M == 0:
-		return True;
-
-	if (M == 1) and (bitwise_or(A).bit_length() == bit):
 		return True
 
 	bitwise = A[0]
 	for i in range(0, len(A)-1):
-		#print 'bit : ', (bitwise & bitwise_or(A[i+1:])).bit_length()
-		if ((bitwise & bitwise_or(A[i+1:])).bit_length() == bit):
-			return possible_max_bit(bit, A[i+1:], M-1)
+		if (bin(bitwise & bitwise_or(A[i+1:])).count("1") == bit):
+			print A[0:i+1], '|', A[i+1:]
+			return find_max_setbit(bit, A[i+1:], M-1)
 		else:
-			bitwise = bitwise | A[i]
+			bitwise = bitwise | A[i+1]
 
-	return False
+	if (M == 1) and (bin(bitwise_or(A)).count("1") == bit):
+		print A, '|'
+		return True	
+
+	return False	
 
 def starforce(A, M):
-	max_bit = count_bitwise_or(A)
-	#print 'max : ', max_bit
+	
+	print_binary(A)
+
+	max_bit = count_setbit(A)
+	print 'max : ', max_bit
 
 	for i in range(max_bit, 0, -1):
-		if possible_max_bit(i, A, M):
+		print '->', i
+		if find_max_setbit(i, A, M):
 			return i
 	return 0
 
+def print_binary(A):
+	for i in A:
+		print i, bin(i)
+
 def test_main():
-	print starforce(A, M)
+	S = '=========================\n'
+	print S, starforce([8, 7, 4, 12 ,2, 1], 3)
+	print S, starforce([4, 2, 1], 2)
+	print S, starforce([255, 8, 7, 4, 1], 2)
+	print S, starforce([65535, 255], 1)
+	print S, starforce([1, 0, 12, 15, 3, 19], 2)
 
 def main():
 	iters = sys.stdin.readline().strip()
@@ -62,5 +68,4 @@ def main():
 		print starforce(A, int(M))
 
 if __name__ == '__main__':
-	main()
-	
+	test_main()
