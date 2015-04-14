6b963d67ad18cd6c7881a692a317c8395a534e80
jiyoung song <itsukis@gmail.com>
2015-04-14 13:32:49 +0900

    Google code jam prob B is solved

--- google/QR2015/p2.py
+++ google/QR2015/p2.py
@@ -11,26 +11,42 @@ def eat(P):
 			NP.append(p-1)
 	return NP
 
-def test(P, t):
-
-	if not P:
+def cakebalance(P, t, maxT):
+	if (not P) or t >= maxT:
 		return t
 
-	tP = list(P)
 	max_t, max_idx = choose_max(P)
+	min_t = []
 	for m in range(int(P[max_idx]/2), -1, -1):
-		tP[max_idx] = tP[max_idx] - m
-		next_max_t = max(tP)
-		if (max_t - next_max_t > 1):
-			tP.append(m)
-			return test(tP, t+1)
-	return test(eat(P), t+1)
+		tP = list(P)
+		if m != 0:
+			tP[max_idx] = P[max_idx] - m
+			tP.append(m)		
+			min_t.append(cakebalance(tP, t+1, maxT))
+		else:
+			min_t.append(cakebalance(eat(P), t+1, maxT))
 
+	return min(min_t)
+
+def unitTest():
+	print cakebalance([6, 6], 0, max([6, 6]))
+	print cakebalance([4], 0, max([4]))
+	print cakebalance([3], 0,  max([3]))
+	print cakebalance([1, 2, 1, 2], 0, max([1, 2, 1, 2]))
 
 def main():
-	print test([4], 0)
-	print test([3], 0)
-	print test([1, 2, 1, 2], 0)
+	result = []
+	iters = sys.stdin.readline().strip()
+	for i in range(0, int(iters)):
+		P = []
+		N = sys.stdin.readline().strip()
+		S = sys.stdin.readline().strip().split()
+		for s in S:
+			P.append(int(s))
+		result.append(cakebalance(P, 0, max(P)))
+	for i in range(0, len(result)):
+		print("Case #%d:" % (i+1)), result[i]
 
 if __name__=='__main__':
+	#unitTest()
 	main()
