47a20fc1caac2fe4fddb2ca95928b7e97c4c6468
jiyoung song <itsukis@gmail.com>
2015-05-13 18:56:37 +0900

    add decision tree module

---
+++ dt/build/lib/DecisionTree/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from DecisionTree import __version__
+from DecisionTree import __author__
+from DecisionTree import __date__
+from DecisionTree import __url__
+from DecisionTree import __copyright__
+
+from DecisionTree import DecisionTree
+
+
