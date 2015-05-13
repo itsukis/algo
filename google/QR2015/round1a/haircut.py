979b4cf8a890718eb5694dd483bfcca9eb238d08
jiyoung song <itsukis@gmail.com>
2015-05-13 20:32:44 +0900

    problem haircut

--- google/QR2015/round1a/haircut.py
+++ google/QR2015/round1a/haircut.py
@@ -2,8 +2,25 @@
 
 import sys
 
-def bin_search():
-	return 0
+def found(N, b, t):
+	cpl = count_completed_cuts(b, t)
+	avb, avbl = count_available_bar(b, t)
+	if N > cpl and N <= cpl + avb:
+		return True, avbl
+	else: 
+		return False
+
+def count_available_bar(b, t):
+	abl = []
+	idx = 0
+	sum = 0
+	for bt in b:
+		if t % bt == 0:
+			sum = sum + 1
+			abl.append(idx)
+		idx = idx + 1
+
+	return sum, abl
 
 def count_completed_cuts(b, t):
 	sum = 0
@@ -14,7 +31,13 @@ def count_completed_cuts(b, t):
 	return sum
 
 def main():
-	print count_completed_cuts([10, 5], 15)
+	bl = [4, 2, 1]
+	t = 4
+
+	print found(8, bl, 3)
+	print count_completed_cuts(bl, t)
+	print count_available_bar(bl, t)
+#	print count_completed_cuts([4, 2, 1], )
 
 if __name__=='__main__':
 	main()
